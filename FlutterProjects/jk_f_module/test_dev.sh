337a25eb84ed755599dcf2898797b71e255b9ccb
lzj0718 <525331283@qq.com>
2021-12-18 23:08:48 +0800

    1

--- FlutterProjects/jk_f_module/test_dev.sh
+++ FlutterProjects/jk_f_module/test_dev.sh
@@ -5,12 +5,12 @@ fi
 echo "准备输出所有文件到目录: $out"
 
 echo "清除所有已编译文件"
-find . -d -name build | xargs rm -rf
-flutter clean
-rm -rf $out
-rm -rf build
+# find . -d -name build | xargs rm -rf
+# flutter clean
+# rm -rf $out
+# rm -rf build
 
-flutter packages get
+# flutter packages get
 
 addFlag(){
     cat .ios/Podfile > tmp1.txt
@@ -20,39 +20,38 @@ addFlag(){
     rm tmp1.txt tmp2.txt
 }
 
-echo "检查 .ios/Podfile文件状态"
-a=$(cat .ios/Podfile)
-if [[ $a == use* ]]; then
-    echo '已经添加use_frameworks, 不再添加'
-else
-    echo '未添加use_frameworks,准备添加'
-    # addFlag
-    echo "添加use_frameworks 完成"
-fi
+# echo "检查 .ios/Podfile文件状态"
+# a=$(cat .ios/Podfile)
+# if [[ $a == use* ]]; then
+#     echo '已经添加use_frameworks, 不再添加'
+# else
+#     echo '未添加use_frameworks,准备添加'
+#     # addFlag
+#     echo "添加use_frameworks 完成"
+# fi
 
 echo "编译flutter"
 # flutter build ios --release --no-codesign
-flutter build ios-framework --output=$out
+# flutter build ios-framework --output=$out
 
 echo "编译flutter完成"
-mkdir $out
+# mkdir $out
 
 # cp -r build/ios/Release-iphoneos/*/*.framework $out
 # cp -r .ios/Flutter/App.framework $out
 # cp -r .ios/Flutter/engine/Flutter.framework $out
 
-echo "复制framework库到临时文件夹: $out"
 
 libpath='../../JKFlutterKits/iOS'
 
-sudo rm -rf "$libpath/ios_frameworks"
-sudo mkdir "$libpath/ios_frameworks"
+rm -rf "$libpath/ios_frameworks"
+mkdir "$libpath/ios_frameworks"
 for file in $out/Release/*; 
 do 
     for subfile in $file/ios-arm64_armv7/*; 
     do
         case "$subfile" in
-	        *.framework ) sudo cp -r "$subfile" "$libpath/ios_frameworks";;
+	        *.framework ) cp -r "$subfile" "$libpath/ios_frameworks";;
         esac
         
     done
